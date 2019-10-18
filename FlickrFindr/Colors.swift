f457dfba0b472259a2267557f93b787a927cf5d7
Julian Panucci <julpanucci@gmail.com>
2019-10-18 11:41:16 -0400

    Added colors file

---
+++ FlickrFindr/Colors.swift
@@ -0,0 +1,13 @@
+//
+//  Colors.swift
+//  FlickrFindr
+//
+//  Created by Julian Panucci on 10/18/19.
+//  Copyright © 2019 Panucci. All rights reserved.
+//
+
+import UIKit
+
+struct Colors {
+    static let blue = UIColor(red: 0.05, green: 0.20, blue: 0.58, alpha: 1.00)
+}
