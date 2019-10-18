ca35f4872990512b3448edf4e05085970fc441eb
Julian Panucci <julpanucci@gmail.com>
2019-10-17 23:29:14 -0400

    Added Strings file

---
+++ FlickrFindr/Strings.swift
@@ -0,0 +1,17 @@
+//
+//  Strings.swift
+//  FlickrFindr
+//
+//  Created by Julian Panucci on 10/17/19.
+//  Copyright © 2019 Panucci. All rights reserved.
+//
+
+import Foundation
+
+struct Strings {
+    
+    struct Keys {
+        static let flickr = "1508443e49213ff84d566777dc211f2a"
+    }
+    
+}
