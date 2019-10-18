310595ece97e88bbb2e1d48f15471ae5017b51a9
Julian Panucci <julpanucci@gmail.com>
2019-10-17 23:28:44 -0400

    Created Photo model

---
+++ FlickrFindr/Photo.swift
@@ -0,0 +1,62 @@
+//
+//  Photo.swift
+//  FlickrFindr
+//
+//  Created by Julian Panucci on 10/17/19.
+//  Copyright © 2019 Panucci. All rights reserved.
+//
+
+import Foundation
+
+class FlickrSearchResponse: Decodable {
+    private enum CodingKeys: String, CodingKey {
+        case photosResponse = "photos"
+        case status = "stat"
+    }
+    var photosResponse: PhotosResponse?
+    var status: String?
+}
+
+class PhotosResponse: Decodable {
+    private enum CodingKeys: String, CodingKey {
+        case page = "page"
+        case pages = "pages"
+        case perpage = "perpage"
+        case total = "total"
+        case photos = "photo"
+    }
+    
+    var page: Int?
+    var pages: Int?
+    var perpage: Int?
+    var total: String?
+    var photos: [Photo]?
+}
+
+class Photo: Decodable {
+    private enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case title = "title"
+        case imageString = "url_o"
+        case thumbnailString = "url_t"
+    }
+    
+    var id: String?
+    var title: String?
+    private var imageString: String?
+    private var thumbnailString: String?
+    
+    var imageURL: URL? {
+        if let imageString = self.imageString {
+            return URL(string: imageString)
+        }
+        return nil
+    }
+    
+    var thumbnailURL: URL? {
+        if let thumbString = self.thumbnailString {
+            return URL(string: thumbString)
+        }
+        return nil
+    }
+}
