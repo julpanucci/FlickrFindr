af265872dc595592c40d23f196fcd012d9f9cd9b
Julian Panucci <julpanucci@gmail.com>
2019-10-24 09:08:23 -0400

    Update colors and strings

--- FlickrFindr/Constants/Colors.swift
+++ FlickrFindr/Constants/Colors.swift
@@ -10,4 +10,5 @@ import UIKit
 
 struct Colors {
     static let blue = UIColor(red: 0.05, green: 0.20, blue: 0.58, alpha: 1.00)
+    static let orange = UIColor(red: 0.89, green: 0.45, blue: 0.12, alpha: 1.00)
 }
