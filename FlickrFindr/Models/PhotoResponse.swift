215b2b368917ecbf72cccfb606b33c714e1139a6
Julian Panucci <julpanucci@gmail.com>
2019-10-27 16:07:48 -0400

    Created supporting files group

---
+++ FlickrFindr/Models/PhotoResponse.swift
@@ -0,0 +1,9 @@
+//
+//  PhotoResponse.swift
+//  FlickrFindr
+//
+//  Created by Julian Panucci on 10/27/19.
+//  Copyright © 2019 Panucci. All rights reserved.
+//
+
+import Foundation
---
+++ FlickrFindr/Models/Search.swift
@@ -0,0 +1,9 @@
+//
+//  Search.swift
+//  FlickrFindr
+//
+//  Created by Julian Panucci on 10/27/19.
+//  Copyright © 2019 Panucci. All rights reserved.
+//
+
+import Foundation
